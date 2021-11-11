91f6b683eda1ad0840637b728e7b55152072b4af
Eleus Apon <eleus.apon@hotmail.com>
2021-11-11 19:49:51 +0600

    createing security group

---
+++ sec-group.tf
@@ -0,0 +1,13 @@
+resource "aws_security_group" "test-security-group" {
+  # ... other configuration ...
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+}
