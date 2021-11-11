ff3d6df0885524abe112dc43e481f3402645465e
Eleus Apon <eleus.apon@hotmail.com>
2021-11-11 19:17:08 +0600

    ...

--- state.tf
+++ state.tf
@@ -1,11 +1,9 @@
 terraform{
     backend "s3" {
         bucket = "terraform-cicd-pipeline-dev"
-        encrypt = true
         key = "terraform.tfstate"
         region = "eu-west-1"
-        access_key = "AKIAZUFID5UAWNJJUBRE"
-        secret_key = "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"
+
     }
 }
 
