d131329b243f9d7efe545965d1032999ee9b9c90
Eleus Apon <eleus.apon@hotmail.com>
2021-11-16 18:55:27 +0600

    ...

--- test-vpc.tf
+++ test-vpc.tf
@@ -1,3 +1,8 @@
-resource "aws_vpc" "default" {
-  cidr_block = "10.1.1.0/16"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.1.1.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
 }
